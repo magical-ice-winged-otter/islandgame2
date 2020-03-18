87667878970b0a0552d75319a8240db015ec8ba3
Evan <pokevoyager0@gmail.com>
2020-03-18 16:14:20 -0600

    update new game settings

--- src/new_game.c
+++ src/new_game.c
@@ -128,7 +128,7 @@ static void ClearFrontierRecord(void)
 
 static void WarpToTruck(void)
 {
-    SetWarpDestination(MAP_GROUP(FALLARBOR_TOWN), MAP_NUM(FALLARBOR_TOWN), -1, -1, -1);
+    SetWarpDestination(MAP_GROUP(INSIDE_OF_TRUCK), MAP_NUM(INSIDE_OF_TRUCK), -1, -1, -1);
     WarpIntoMap();
 }
 
--- src/overworld.c
+++ src/overworld.c
@@ -1522,7 +1522,7 @@ void CB2_NewGame(void)
     PlayTimeCounter_Start();
     ScriptContext1_Init();
     ScriptContext2_Disable();
-    gFieldCallback = NULL;
+    gFieldCallback = ExecuteTruckSequence;
     gFieldCallback2 = NULL;
     DoMapLoadLoop(&gMain.state);
     SetFieldVBlankCallback();
