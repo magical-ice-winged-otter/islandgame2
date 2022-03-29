3782cdfb4e8b06cab909e360b816ab730bbefae9
ghoulslash <pokevoyager0@gmail.com>
2022-03-28 20:13:57 -0400

    fix setfollower for real

--- src/scrcmd.c
+++ src/scrcmd.c
@@ -2314,7 +2314,7 @@ bool8 ScrCmd_warpwhitefade(struct ScriptContext *ctx)
 bool8 ScrCmd_setfollower(struct ScriptContext *ctx)
 {
     u8 localId = ScriptReadByte(ctx);
-    u16 flags = ScriptReadByte(ctx);
+    u16 flags = ScriptReadHalfword(ctx);
     
     SetUpFollowerSprite(localId, flags);
     return FALSE;
