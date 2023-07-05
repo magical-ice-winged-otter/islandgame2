00822e51aba249501e60b1813454f484f4d12cf2
Daniel Walls <walls.dk@gmail.com>
2023-07-05 16:50:25 -0700

    created front lawn map

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1059,3 +1059,5 @@ Common_EventScript_LegendaryFlewAway::
 	.include "data/text/frontier_brain.inc"
 	.include "data/text/save.inc"
 	.include "data/text/birch_speech.inc"
+
+	.include "data/maps/ForestBaseCamp_FrontLawn/scripts.inc"
