3669c6a35cc0d1a92b1d7f4daba5ca14fd5f62ba
Daniel Walls <walls.dk@gmail.com>
2023-07-06 23:35:31 -0700

    re-combined decl and definitions for modern make

--- src/islandgame_custom_intro.c
+++ src/islandgame_custom_intro.c
@@ -14,18 +14,14 @@ static const u8 sName[] = _(ISLANDGAME_PLAYER_NAME);
 // This gets called after the intro cutscene, right when the new game is started.
 void IslandGameCustomStartup()
 {
-    u16 species;
-    u8 level;
-    u16 item;
-
     // This flag makes sure that we unlock the pokemon selection menu:
     // usually its set when you pick the starter.
     FlagSet(FLAG_SYS_POKEMON_GET);
 
     // todo: this actually is a traded mon technically, and won't obey orders
-    species = ISLANDGAME_STARTING_MON;
-    level = ISLANDGAME_STARTING_MON_LEVEL;
-    item = ISLANDGAME_STARTING_MON_ITEM;
+    u16 species = ISLANDGAME_STARTING_MON;
+    u8 level = ISLANDGAME_STARTING_MON_LEVEL;
+    u16 item = ISLANDGAME_STARTING_MON_ITEM;
     ScriptGiveMon(species, level, item, 0, 0, 0);
 
     // I'm honestly not sure why you need the weird syntax around string literals,
@@ -36,9 +32,7 @@ void IslandGameCustomStartup()
 // Almost exactly copied from src/main_menu.c
 static void SetPlayerName(const u8 *name)
 {
-    u8 i;
-
-    for (i = 0; i < PLAYER_NAME_LENGTH; i++)
+    for (u8 i = 0; i < PLAYER_NAME_LENGTH; i++)
     {
         gSaveBlock2Ptr->playerName[i] = name[i];
     }
