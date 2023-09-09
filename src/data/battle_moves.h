ab768e1234c69930d440913b930065bef3691a3d
rain960 <lordraindance5@gmail.com>
2023-09-09 19:58:08 -0400

    made shadow blast shadow type

--- src/data/battle_moves.h
+++ src/data/battle_moves.h
@@ -13660,7 +13660,7 @@ const struct BattleMove gBattleMoves[MOVES_COUNT_Z] =
     {
         .effect = EFFECT_HIT,
         .power = 140,
-        .type = TYPE_DARK,
+        .type = TYPE_SHADOW,
         .accuracy = 100,
         .pp = 5,
         .secondaryEffectChance = 10,
