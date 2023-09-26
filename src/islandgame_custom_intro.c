81894ff3e48f6a5ae0768a937f338e63e46cc289
rain960 <lordraindance5@gmail.com>
2023-09-26 18:50:02 -0400

    Fixed water tile on top

--- src/islandgame_custom_intro.c
+++ src/islandgame_custom_intro.c
@@ -62,6 +62,11 @@ void IslandGameCustomStartup()
         //debug 2: CHERIPORT LOCATION
         FlagToggle(FLAG_RECEIVED_RUNNING_SHOES);
         FlagToggle(FLAG_SYS_B_DASH);
+        FlagToggle(FLAG_LEFT_SHIP);
+        //FlagToggle(FLAG_PROF_GAVE_EEVEE);
+
+        //debug sidequest:
+        //FlagToggle(FLAG_ROOM3_GAVE_WATER);
 
         //See: data/scripts/islandgame_debug[.pory/.inc]
         ScriptContext_SetupScript(IslandGame_LoadIn_Message);
