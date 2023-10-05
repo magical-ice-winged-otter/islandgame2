9c52a8e79530a44b757f388e1023dac2be3162ac
lordraindance2 <47706100+lordraindance2@users.noreply.github.com>
2023-10-05 18:43:02 -0400

    Southern oranna path (#24)
    * early version of cheriport
    
    * got aseprite conversion tool working
    
    * added new general tileset
    
    * Allowed sh files to be run outside of the directory
    
    * reintroduced transparency color (pink)
    
    * fixed original corrupted tileset
    
    * Updated porytiles to 0.0.6
    
    * initial add shadow type to data
    
    * added sample shadow type graphic
    
    * made shadow blast shadow type
    
    * fix duplicate running shoes
    
    * shadow dog
    
    * updated cheriport
    
    * updated cheriport
    
    * Added Cheriport popup
    
    * Added cheriport lab + eevee recieve
    
    * Eevee gift has set nature
    
    * set flag flag prof gave eevee as well
    
    * Connected ship to cheriport warp
    
    * Added 'can't leave cheriport without pokemon' trigger
    
    * Added comment on onload debugging
    
    * Added nickname script
    
    * Gave eevee set moves
    
    * Directly fixed lab tiles (temp solution)
    
    * Added an indoor map to house in cheriport
    
    * Added 2 npcs to cheriport
    
    * spicing up cheriport; add poke center and mart
    
    * Revert "initial add shadow type to data"
    
    This reverts commit bb9dfaea7f22cfcc58021db4f04f06aa66a16af5.
    
    * Revert "added sample shadow type graphic"
    
    This reverts commit c2c01fd97c6bd497befa55e804160bba788c76e0.
    
    * Revert: removed shadow blast and shadow dog changes
    
    * fixed warps
    
    * Merge branch 'master' of github.com:lordraindance2/islandgame2 into cheriport
    
    * added house
    
    * very primitive commit
    
    * Added basic area
    
    * work on pokemon lab
    
    * Adjusted connection
    
    * Fixed connection
    
    * Fixed layers
    
    * Fixed collision
    
    * Fixed water tile on top
    
    * Fixed lab collision
    
    * final changes to cheriport 4 now
    
    * Added Pokedex and pokeball give scripts
    
    * Added some wild mon and a trainer battle
    
    * worked on path; made it synced with cheriport
    
    * added another trainer; added more npcs
    
    * very primitive commit
    
    * Added basic area
    
    * Adjusted connection
    
    * Fixed connection
    
    * Fixed collision
    
    * Added some wild mon and a trainer battle
    
    * worked on path; made it synced with cheriport
    
    * added another trainer; added more npcs
    
    * finished southern oranna path for now
    
    * fixed the trainers
    
    * fixed a trainer in the south path
    
    * fix collision error
    
    * Added popup name when entering map
    
    ---------
    
    Co-authored-by: Daniel Walls <walls.dk@gmail.com>
    Co-authored-by: ressykins <resistantpwnage@gmail.com>

--- src/data/trainer_parties.h
+++ src/data/trainer_parties.h
@@ -11,41 +11,41 @@ static const struct TrainerMon sParty_Sawyer1[] = {
     }
 };
 
-static const struct TrainerMon sParty_GruntAquaHideout1[] = {
+static const struct TrainerMon sParty_SOrannaPathTrainer1[] = {
     {
-    .lvl = 32,
-    .species = SPECIES_POOCHYENA,
+    .lvl = 5,
+    .species = SPECIES_STARLY,
     }
 };
 
-static const struct TrainerMon sParty_GruntAquaHideout2[] = {
+static const struct TrainerMon sParty_SOrannaPathTrainer2[] = {
     {
-    .lvl = 31,
-    .species = SPECIES_ZUBAT,
+    .lvl = 4,
+    .species = SPECIES_AZURILL,
     },
     {
-    .lvl = 31,
-    .species = SPECIES_CARVANHA,
+    .lvl = 5,
+    .species = SPECIES_PACHIRISU,
     }
 };
 
-static const struct TrainerMon sParty_GruntAquaHideout3[] = {
+static const struct TrainerMon sParty_SOrannaPathTrainer3[] = {
     {
-    .lvl = 32,
-    .species = SPECIES_ZUBAT,
+    .lvl = 6,
+    .species = SPECIES_SURSKIT,
     }
 };
 
-static const struct TrainerMon sParty_GruntAquaHideout4[] = {
+static const struct TrainerMon sParty_SOrannaPathTrainer4[] = {
     {
-    .lvl = 32,
-    .species = SPECIES_CARVANHA,
+    .lvl = 6,
+    .species = SPECIES_SKIDDO,
     }
 };
 
 static const struct TrainerMon sParty_GruntSeafloorCavern1[] = {
     {
-    .lvl = 36,
+    .lvl = 7,
     .species = SPECIES_POOCHYENA,
     }
 };
