778f16150bf1481bb10ebe11c2e5eebb1eb525ec
Daniel Walls <walls.dk@gmail.com>
2023-11-14 11:38:15 -0800

    Events and polish (#38)
    * started berry quest guy (and cleaned up quests)
    
    * added berry sidequest to persi berry maniac
    
    * cleaned up movement scripts in persi
    
    * fixed animation errors w/ islandgame general
    
    * buffered more space for quests, direct initialize array, moved strings

--- include/constants/quests.h
+++ include/constants/quests.h
@@ -14,73 +14,26 @@
 #define QUEST_MENU_CHECK_COMPLETE       9   //checks if questId has been completed. Returns result to gSpecialVar_Result
 #define QUEST_MENU_BUFFER_QUEST_NAME    10   //buffers a quest name to gStringVar1
 
-// quest number defines
+
+// Custom Quests
 #define QUEST_PERSI_FAVORITE_CARD          0
-#define QUEST_2                            1
-#define QUEST_3                            2
-#define QUEST_4                            3
-#define QUEST_5                            4
-#define QUEST_6                            5
-#define QUEST_7                            6
-#define QUEST_8                            7
-#define QUEST_9                            8
-#define QUEST_10                           9
-#define QUEST_11                          10
-#define QUEST_12                          11
-#define QUEST_13                          12
-#define QUEST_14                          13
-#define QUEST_15                          14
-#define QUEST_16                          15
-#define QUEST_17                          16
-#define QUEST_18                          17
-#define QUEST_19                          18
-#define QUEST_20                          19
-#define QUEST_21                          20
-#define QUEST_22                          21
-#define QUEST_23                          22
-#define QUEST_24                          23
-#define QUEST_25                          24
-#define QUEST_26                          25
-#define QUEST_27                          26
-#define QUEST_28                          27
-#define QUEST_29                          28
-#define QUEST_30                          29
-#define QUEST_COUNT           (QUEST_30 + 1)
+#define QUEST_PERSI_BERRY_MANIAC           1
+#define QUEST_COUNT                        2 // This number is literally how many to show in the UI (keep it accurate!)
 
-#define SUB_QUEST_1          0
-#define SUB_QUEST_2          1
-#define SUB_QUEST_3          2
-#define SUB_QUEST_4          3
-#define SUB_QUEST_5          4
-#define SUB_QUEST_6          5
-#define SUB_QUEST_7          6
-#define SUB_QUEST_8          7
-#define SUB_QUEST_9          8
-#define SUB_QUEST_10         9
-#define SUB_QUEST_11        10
-#define SUB_QUEST_12        11
-#define SUB_QUEST_13        12
-#define SUB_QUEST_14        13
-#define SUB_QUEST_15        14
-#define SUB_QUEST_16        15
-#define SUB_QUEST_17        16
-#define SUB_QUEST_18        17
-#define SUB_QUEST_19        18
-#define SUB_QUEST_20        19
-#define SUB_QUEST_21        20
-#define SUB_QUEST_22        21
-#define SUB_QUEST_23        22
-#define SUB_QUEST_24        23
-#define SUB_QUEST_25        24
-#define SUB_QUEST_26        25
-#define SUB_QUEST_27        26
-#define SUB_QUEST_28        27
-#define SUB_QUEST_29        28
-#define SUB_QUEST_30        29
+// Custom Sub-Quests
+#define SUB_QUEST_PERSI_BERRY_1            0
+#define SUB_QUEST_PERSI_BERRY_2            1
+#define SUB_QUEST_PERSI_BERRY_3            2
+#define SUB_QUEST_PERSI_BERRY_4            3
+#define SUB_QUEST_PERSI_BERRY_5            4
+#define SUB_QUEST_PERSI_BERRY_6            5
+#define SUB_QUEST_COUNT_PERSI_BERRY        6
+#define SUB_QUEST_COUNT                    7 // This number is literally how many to show in the UI (keep it accurate!)
 
-#define QUEST_1_SUB_COUNT 10
-#define QUEST_2_SUB_COUNT 20
-#define SUB_QUEST_COUNT (QUEST_1_SUB_COUNT + QUEST_2_SUB_COUNT)
+// #define QUEST_ARRAY_COUNT (SUB_QUEST_COUNT > QUEST_COUNT ? SUB_QUEST_COUNT : QUEST_COUNT)
 
-#define QUEST_ARRAY_COUNT (SUB_QUEST_COUNT > QUEST_COUNT ? SUB_QUEST_COUNT : QUEST_COUNT)
+// We want to allocate more space in the saveblock for quests than we need,
+// so we don't make breaking changes in the future.
+#define QUEST_ARRAY_COUNT 100
+
 #endif // GUARD_CONSTANTS_QUESTS_H
