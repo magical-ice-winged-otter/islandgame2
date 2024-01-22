da98989a07d10954ecbafa22f55c4c80c79d55b2
poetahto <walls.dk@gmail.com>
2024-01-22 13:25:12 -0800

    more work on save

--- src/islandgame_save_upgrade.c
+++ src/islandgame_save_upgrade.c
@@ -26,15 +26,13 @@ static u32 GetSaveBlock1Offset(u8* address)
     return (u32) (address - (u8*) gSaveBlock1Ptr);
 }
 
-static void SaveBlock1IncreaseArraySize(void* arrayStart, u32 oldArraySize, u32 newArraySize, u32 oldSaveBlockSize)
+static void SaveBlock1IncreaseArraySize(void* array, u32 oldArraySize, u32 newArraySize, u32 oldSaveBlockSize)
 {
     u8* arrayStart;
-    u8* oldSaveBlockEnd;
     u8* oldArrayEnd;
     u8* newArrayEnd;
 
-    arrayStart = (u8*) arrayStart;
-    oldSaveBlockEnd = (u8*) gSaveBlock1Ptr + oldSaveBlockSize;
+    arrayStart = (u8*) array;
     oldArrayEnd = arrayStart + oldArraySize;
     newArrayEnd = arrayStart + newArraySize;
 
@@ -49,13 +47,22 @@ static void SaveBlock1IncreaseArraySize(void* arrayStart, u32 oldArraySize, u32 
 
 static void UpgradeSaveV0ToV1()
 {
+    u32 saveBlockSize = 13172;
+
     DebugPrintf("Upgrading save from v0 to v1");
+    DebugPrintf("dex size: %u", sizeof(gSaveBlock1Ptr->dexSeen));
+    DebugPrintf("item size: %u", sizeof(gSaveBlock1Ptr->bagPocket_Items));
     DEBUG_SAVEBLOCK1(bagPocket_Items);
     DEBUG_SAVEBLOCK1(savedMusic);
     DEBUG_SAVEBLOCK1(saveVersion);
     // breaking changes:
-    // bag items increased 30 -> 96
-    // pokemon count increased ? -> ? (gen 9)
+
     // match call people inserted into block
-    // SaveBlock1IncreaseArraySize(&gSaveBlock1Ptr->bagPocket_Items, 30, 93, );
+
+    // bag items increased 30 -> 96
+    SaveBlock1IncreaseArraySize(&gSaveBlock1Ptr->bagPocket_Items, 30, 93, saveBlockSize);
+
+    // pokemon count increased ? -> ? (gen 9)
+    SaveBlock1IncreaseArraySize(&gSaveBlock1Ptr->dexSeen, 114, 129, saveBlockSize);
+    SaveBlock1IncreaseArraySize(&gSaveBlock1Ptr->dexCaught, 114, 129, saveBlockSize);
 }
