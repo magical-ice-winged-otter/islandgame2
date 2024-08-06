a6f93207cd1f0c2c528122a4ef5f149880061d31
Bivurnum <147376167+Bivurnum@users.noreply.github.com>
2024-08-05 20:29:11 -0500

    Update global.h

--- include/constants/global.h
+++ include/constants/global.h
@@ -172,6 +172,9 @@
 #define CONNECTION_DIVE     5
 #define CONNECTION_EMERGE   6
 
+#define SIDEWAYS_STAIRS_IMPLEMENTED         FALSE
+#define POST_BATTLE_FOLLOWER_FIX            FALSE   //if you experience the follower de-syncing with the player after battle, set to TRUE
+
 #if TESTING
 #include "config/test.h"
 #endif
