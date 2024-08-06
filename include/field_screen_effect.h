01d01b877c780752a5d253aa8b0e1f0a109b98a3
Bivurnum <147376167+Bivurnum@users.noreply.github.com>
2024-08-05 20:46:38 -0500

    Update field_screen_effect.h

--- include/field_screen_effect.h
+++ include/field_screen_effect.h
@@ -41,5 +41,8 @@ void DoOrbEffect(void);
 void FadeOutOrbEffect(void);
 void WriteFlashScanlineEffectBuffer(u8 flashLevel);
 bool8 IsPlayerStandingStill(void);
+void SetPlayerVisibility(bool8 visible);
+void Task_WarpAndLoadMap(u8 taskId);
+void Task_DoDoorWarp(u8 taskId);
 
 #endif // GUARD_FIELD_SCREEN_EFFECT_H
