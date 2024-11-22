7174d5e3dbe4ec1530b029821169e788b016b58a
rain960 <lordraindance5@gmail.com>
2024-11-22 15:57:13 -0500

    Implemented guards for rtc.h

--- include/constants/rtc.h
+++ include/constants/rtc.h
@@ -1,4 +1,9 @@
+#ifndef GUARD_CONSTANTS_RTC_H
+#define GUARD_CONSTANTS_RTC_H
+
 #define TIME_MORNING           0
 #define TIME_DAY               1
 #define TIME_EVENING           2
 #define TIME_NIGHT             3
+
+#endif // GUARD_CONSTANTS_RTC_H 
